5223c76718fd55aa65ed38824fc986fcdf24e4b8
AtilioA <atiliogpg@protonmail.com>
2024-03-26 17:03:46 -0300

    refactor: add stub for adding mod items according to `PersistentVars`

--- AV Item Shipment Framework/Mods/AV Item Shipment Framework/ScriptExtender/Lua/Shared/EventHandlers.lua
+++ AV Item Shipment Framework/Mods/AV Item Shipment Framework/ScriptExtender/Lua/Shared/EventHandlers.lua
@@ -10,6 +10,25 @@ function EHandlers.OnLevelGameplayStarted(levelName, isEditorMode)
     -- TODO: Set variable to deliver on Act 1
     return
   end
+
+  -- Ensure PersistentVars table is initialized
+  ItemShipmentInstance:InitializePVars()
+  -- if Mods.AVItemShipmentFramework.PersistentVars == nil then
+  --   Mods.AVItemShipmentFramework.PersistentVars = {}
+  -- end
+
+  -- Iterate through each mod and check if items need to be added
+  for modGUID, modData in pairs(ItemShipmentInstance.mods) do
+    if Ext.ModIsLoaded(modGUID) then
+      if not Mods.AVItemShipmentFramework.PersistentVars.shipments[modGUID] then
+        -- TODO: Add items according to the config
+        ISFDebug(1, "Adding items for mod: " .. Ext.Mod.GetMod(modGUID).Info.Name)
+
+        -- Update PersistentVars to track added items
+        Mods.AVItemShipmentFramework.PersistentVars.shipments[modGUID] = true
+      end
+    end
+  end
 end
 
 return EHandlers
