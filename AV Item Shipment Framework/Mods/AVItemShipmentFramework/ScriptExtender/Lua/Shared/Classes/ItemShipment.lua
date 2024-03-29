a3ff80f444a67216a45d21a6681c03739196495d
Atílio <atiliodadalto@protonmail.com>
2024-03-29 01:28:11 -0300

    feat: add checks for item existence in camp chests (mailboxes) and party members' inventories

--- AV Item Shipment Framework/Mods/AVItemShipmentFramework/ScriptExtender/Lua/Shared/Classes/ItemShipment.lua
+++ AV Item Shipment Framework/Mods/AVItemShipmentFramework/ScriptExtender/Lua/Shared/Classes/ItemShipment.lua
@@ -248,22 +248,65 @@ end
 
 function ItemShipment:CheckExistence(ISFModVars, modGUID, item)
   -- Check if the item has already been added
+  ISFWarn(2, "CHECKING MODVARS")
   if item.Send.CheckExistence.FrameworkCheck then
     if ISFModVars.Shipments[modGUID][item.TemplateUUID] == true then
       return true
     end
   end
 
-  -- -- Implement additional checks based on the config
-  -- if item.Send.CheckExistence.PartyMembers.Followers then
-  --   -- TODO: Check if party members have the item, for each party member
-  -- end
-
-  if item.Send.CheckExistence.CampChest.Player1Chest then
-    -- VCHelpers.Inventory:GetInventory()
-    if VCHelpers.Inventory:IsItemInCampChest(item.TemplateUUID) then
-      return true
+  -- Check if the item exists in the camp chests
+  ISFWarn(2, "CHECKING CAMP CHESTS")
+  if item.Send.CheckExistence.CampChest then
+    if item.Send.CheckExistence.CampChest.Player1Chest then
+      -- FIXME: check mailbox instead
+      if VCHelpers.Inventory:GetItemTemplateInInventory(item.TemplateUUID, ISFModVars.Mailboxes["65537"]) ~= nil then
+        ISFWarn(1, "Item already exists in the inventory of a camp chest and will not be shipped.")
+        return true
+      end
     end
+
+    if item.Send.CheckExistence.CampChest.Player2Chest then
+      if VCHelpers.Inventory:GetItemTemplateInInventory(item.TemplateUUID, ISFModVars.Mailboxes["65538"]) ~= nil then
+        ISFWarn(1, "Item already exists in the inventory of a camp chest and will not be shipped.")
+        return true
+      end
+    end
+
+    if item.Send.CheckExistence.CampChest.Player3Chest then
+      if VCHelpers.Inventory:GetItemTemplateInInventory(item.TemplateUUID, ISFModVars.Mailboxes["65539"]) ~= nil then
+        ISFWarn(1, "Item already exists in the inventory of a camp chest and will not be shipped.")
+        return true
+      end
+    end
+
+    if item.Send.CheckExistence.CampChest.Player4Chest then
+      if VCHelpers.Inventory:GetItemTemplateInInventory(item.TemplateUUID, ISFModVars.Mailboxes["65540"]) ~= nil then
+        ISFWarn(1, "Item already exists in the inventory of a camp chest and will not be shipped.")
+        return true
+      end
+    end
+  end
+
+  ISFWarn(2, "CHECKING PARTY MEMBERS")
+  if item.Send.CheckExistence.PartyMembers ~= nil then
+    local partyMembers = {}
+    if item.Send.CheckExistence.PartyMembers.AtCamp == true then
+      partyMembers = VCHelpers.Party:GetAllPartyMembers()
+    elseif item.Send.CheckExistence.PartyMembers.ActiveParty == true then
+      partyMembers = VCHelpers.Party:GetPartyMembers()
+    end
+    for _, partyMember in ipairs(partyMembers) do
+      if VCHelpers.Inventory:GetItemTemplateInInventory(item.TemplateUUID, partyMember) ~= nil then
+        ISFWarn(1,
+          "Item " ..
+          item.TemplateUUID ..
+          " already exists in inventory " ..
+          VCHelpers.Loca:GetDisplayName(partyMember) .. " for mod " .. modGUID .. " and will not be shipped.")
+        return true
+      end
+    end
+    ISFWarn(1, "Item " .. item.TemplateUUID .. " does not exist in any party member's inventory and may be shipped.")
   end
 
   return false
