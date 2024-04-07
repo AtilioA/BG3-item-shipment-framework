c847c70bd45d869f9d3408d7020c59fa30269f79
AtilioA <27153776+AtilioA@users.noreply.github.com>
2024-04-07 15:01:48 -0300

    refactor: improve `DragAndDropPreview` div structure and styling

--- lsx-to-isf-json/src/components/DragAndDropPreview.tsx
+++ lsx-to-isf-json/src/components/DragAndDropPreview.tsx
@@ -1,5 +1,6 @@
-// src/components/DragAndDrop/DragAndDropPreview.tsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
 
 interface DragAndDropPreviewProps {
   jsonOutput: string;
@@ -13,29 +14,38 @@ const DragAndDropPreview: React.FC<DragAndDropPreviewProps> = ({
   handleSaveJSON,
 }) => {
   return (
-    <div className="mt-8 w-full max-h-[50vh] overflow-auto">
-      <hr className="border-gray-600 my-4" />
-      <b className="text-xl mt-4 mb-2">Templates included in the JSON:</b>
-      <ul className="list-disc pl-8 mb-8">
-        {templateNames.map((templateName) => (
-          <li key={templateName}>{templateName}</li>
-        ))}
-      </ul>
-      <button
-        onClick={handleSaveJSON}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-0 rounded"
-      >
-        Save JSON
-      </button>
-      <p className="mt-4 mb-2">JSON preview:</p>
-      <pre
-        onClick={handleSaveJSON}
-        style={{ cursor: 'pointer' }}
-        title="Click to save ISF JSON"
-        className="mt-4 p-4 bg-gray-800 rounded-lg text-white"
-      >
-        {jsonOutput}
-      </pre>
+    <div className="w-full max-w-[50%] mx-4 mt-8">
+      <div className="flex flex-col">
+        <div>
+          <b className="text-xl mt-4 mb-2">Templates included in the JSON:</b>
+          <ul className="list-disc pl-8 mb-8">
+            {templateNames.map((templateName) => (
+              <li key={templateName}>{templateName}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-col">
+          <button
+            onClick={handleSaveJSON}
+            className="bg-[#6f69e0] text-white font-bold py-4 px-4 mt-0 rounded transition-colors duration-150 ease-in-out hover:bg-[#5e59c9] flex items-center justify-center"
+          >
+            <FontAwesomeIcon icon={faSave} className="mr-2" />
+            <span className="text-center text-xl">Save ISF JSON</span>
+          </button>
+
+          <p className="mt-4 mb-2">JSON preview:</p>
+          <div className="max-h-[40vh] rounded-[4px] overflow-auto">
+            <pre
+              onClick={handleSaveJSON}
+              style={{ cursor: 'pointer' }}
+              title="Click to save ISF JSON"
+              className="p-4 bg-gray-800 rounded-lg text-white"
+            >
+              {jsonOutput}
+            </pre>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
