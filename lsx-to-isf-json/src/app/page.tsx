5f924ed0ec470c8bf3d98292a8fc1c0484672c5a
AtilioA <27153776+AtilioA@users.noreply.github.com>
2024-04-06 21:04:36 -0300

    refactor: improve project structure

--- lsx-to-isf-json/src/app/page.tsx
+++ lsx-to-isf-json/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import React from 'react';
-import { XmlToJsonComponent } from './XmlToJsonComponent';
+import { XmlToJsonComponent } from '../components/DragAndDrop';
 
 const Page: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl items-center align-center font-bold mb-4 mt-0">LSX to ISF config JSON converter</h1>
+      <h1 className="text-3xl items-center align-center font-bold mb-4 mt-0">ISF config JSON generator</h1>
       <XmlToJsonComponent />
     </div>
   );
--- lsx-to-isf-json/src/components/DragAndDrop.tsx
+++ lsx-to-isf-json/src/components/DragAndDrop.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { FileRejection, useDropzone } from 'react-dropzone';
-import { constructJSON, parseXML } from './utils/xmlToJson';
+import { constructJSON, parseXML } from '../services/xmlToJson';
+import FolderDropComponent from './DragDropFolder';
 
 export const XmlToJsonComponent: React.FC = () => {
   const [jsonOutput, setJsonOutput] = useState('');
@@ -48,35 +49,38 @@ export const XmlToJsonComponent: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full max-w-3xl">
-      <div
-        {...getRootProps()}
-        onPaste={handlePaste}
-        className="w-full h-64 p-8 border-2 border-dashed border-blue-500 rounded-lg cursor-pointer flex items-center justify-center"
-      >
-        <input {...getInputProps()} />
-        <p className="text-lg text-white">Drag and drop an LSX file here, or paste its content.</p>
-      </div>
-      {jsonOutput && (
-        <div className="mt-8 w-full max-h-[50vh] overflow-auto">
-          <button
-            onClick={handleSaveJSON}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Save JSON
-          </button>
-          <hr className="border-gray-600 my-4" />
-          <p className='mt-4 mb-2'>JSON preview:</p>
-          <pre
-            onClick={handleSaveJSON}
-            style={{ cursor: 'pointer' }}
-            title="Click to save ISF JSON"
-            className="mt-4 p-4 bg-gray-800 rounded-lg text-white"
-          >
-            {jsonOutput}
-          </pre>
+    <>
+      <div className="flex flex-col items-center justify-center w-full max-w-3xl">
+        <div
+          {...getRootProps()}
+          onPaste={handlePaste}
+          className="w-full h-64 p-8 border-2 border-dashed border-blue-500 rounded-lg cursor-pointer flex items-center justify-center"
+        >
+          <input {...getInputProps()} />
+          <p className="text-lg text-white">Drag and drop an LSX file here, or paste its content.</p>
         </div>
-      )}
-    </div>
+        {jsonOutput && (
+          <div className="mt-8 w-full max-h-[50vh] overflow-auto">
+            <button
+              onClick={handleSaveJSON}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Save JSON
+            </button>
+            <hr className="border-gray-600 my-4" />
+            <p className='mt-4 mb-2'>JSON preview:</p>
+            <pre
+              onClick={handleSaveJSON}
+              style={{ cursor: 'pointer' }}
+              title="Click to save ISF JSON"
+              className="mt-4 p-4 bg-gray-800 rounded-lg text-white"
+            >
+              {jsonOutput}
+            </pre>
+          </div>
+        )}
+      </div>
+      <FolderDropComponent />
+    </>
   );
 };
