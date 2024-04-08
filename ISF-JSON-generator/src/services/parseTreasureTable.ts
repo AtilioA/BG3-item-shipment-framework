9721599e73cd866c94ec9f64786d70d1e873ee58
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-08 20:50:09 -0300

    fix: check treasure table against all vanilla treasure tables

--- ISF-JSON-generator/src/services/parseTreasureTable.ts
+++ ISF-JSON-generator/src/services/parseTreasureTable.ts
@@ -1,28 +1,34 @@
+import { VANILLA_TREASURE_TABLES } from "@/data/vanillaTreasureTables";
+
 export interface TreasureItem {
-  treasureName: string;
-  templateName: string;
+    treasureName: string;
+    templateName: string;
 };
 
+export function isVanillaTreasureTable(treasureTable: string): boolean {
+    return VANILLA_TREASURE_TABLES.includes(treasureTable);
+}
+
 export function parseTreasureTableData(fileContent: string): TreasureItem[] {
-  const lines = fileContent.split('\n');
-  const treasureItems: TreasureItem[] = [];
-  let currentTreasureTable = '';
+    const lines = fileContent.split('\n');
+    const treasureItems: TreasureItem[] = [];
+    let currentTreasureTable = '';
 
-  lines.forEach(line => {
-    // Check if the line denotes a new treasure table
-    if (line.startsWith('new treasuretable')) {
-      currentTreasureTable = line.split('"')[1]; // Extract the treasure table name
-    } else if (line.startsWith('object category') && currentTreasureTable !== 'TUT_Chest_Potions') {
-      // Extract the first string after 'object category', ignoring 'TUT_Chest_Potions' table
-      // console.log(currentTreasureTable)
-      const parts = line.split('"');
-      if (parts.length >= 2) {
-        const treasureName = parts[1];
-        const templateName = treasureName.startsWith('I_') ? treasureName.substring(2) : treasureName;
-        treasureItems.push({ treasureName, templateName });
-      }
-    }
-  });
+    lines.forEach(line => {
+        // Check if the line denotes a new treasure table
+        if (line.startsWith('new treasuretable')) {
+            currentTreasureTable = line.split('"')[1]; // Extract the treasure table name
+        } else if (line.startsWith('object category') && !isVanillaTreasureTable(currentTreasureTable)) {
+            // Extract the first string after 'object category', ignoring 'TUT_Chest_Potions' table
+            // console.log(currentTreasureTable)
+            const parts = line.split('"');
+            if (parts.length >= 2) {
+                const treasureName = parts[1];
+                const templateName = treasureName.startsWith('I_') ? treasureName.substring(2) : treasureName;
+                treasureItems.push({ treasureName, templateName });
+            }
+        }
+    });
 
-  return treasureItems;
+    return treasureItems;
 }
