a38db5b4cbf6da23f8ec6136a71bd5135d8caf5e
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-07 21:45:20 -0300

    feat: update page title and add ISF icon

--- lsx-to-isf-json/src/app/page.tsx
+++ lsx-to-isf-json/src/app/page.tsx
@@ -4,14 +4,21 @@ import DragAndDropContainer from '@/components/DragAndDrop';
 import Footer from '@/components/Footer';
 import React from 'react';
 
+import Image from 'next/image';
+
 const Page: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl items-center align-center font-bold mb-6 mt-4">ISF config JSON generator</h1>
-      <DragAndDropContainer />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl items-center align-center font-bold mb-6 mt-4">
+                <a href="/" className="hover:text-gray-400 transition-colors duration-150 ease-in-out flex items-center">
+                    <Image src="/Item_CONT_GEN_Mailbox_C.png" alt="ISF icon" width={40} height={40} className="mr-2" />
+                    ISF config JSON generator
+                </a>
+            </h1>
+            <DragAndDropContainer />
+            <Footer />
+        </div>
+    );
 };
 
 export default Page;
