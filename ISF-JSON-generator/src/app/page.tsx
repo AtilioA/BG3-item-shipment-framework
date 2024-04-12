e38ba9cf34382a2d0e3dcaefdb6b2d981c8d3b00
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-04-11 21:57:42 -0300

    refactor(web): update page title

--- ISF-JSON-generator/src/app/page.tsx
+++ ISF-JSON-generator/src/app/page.tsx
@@ -12,7 +12,7 @@ const Page: React.FC = () => {
             <h1 className="text-3xl items-center align-center font-bold mb-6 mt-4">
                 <a href="/" className="text-center hover:text-gray-400 transition-hover flex items-center">
                     <Image src="/Item_CONT_GEN_Mailbox_C.png" alt="ISF icon" width={40} height={40} className="mr-2" />
-                    ISF config JSON generator
+                    Item Shipment Framework config JSON generator
                 </a>
             </h1>
             <div className="flex-1 w-full overflow-y-auto">
