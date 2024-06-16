315c27c2d59689420eccee733d6f8a0fc5d44e88
AtilioA <27153776+AtilioA@users.noreply.github.com>
2024-06-16 01:18:36 -0300

    feat: add confirmation modal after saving JSON file for the first time (checked against localstorage)

--- ISF-JSON-generator/src/components/DragAndDrop.tsx
+++ ISF-JSON-generator/src/components/DragAndDrop.tsx
@@ -176,7 +176,7 @@ const DragAndDropContainer: React.FC = () => {
             return (
                 <div className="flex flex-col items-center text-xl text-gray-400">
                     <p><span className='font-bold'>{modName}</span> has been parsed successfully.</p>
-                    <p className="ml-2 text-base">You can still drop in another mod.</p>
+                    <p className="ml-2 text-base text-gray-500 cursor-default">You can still drop in another mod.</p>
                 </div>
             );
         } else {
--- ISF-JSON-generator/src/components/SaveJSON.tsx
+++ ISF-JSON-generator/src/components/SaveJSON.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSave } from '@fortawesome/free-solid-svg-icons';
+import ConfirmationModal from './ConfirmationModal';
 
 interface SaveJSONButtonProps {
     handleSaveJSON: () => void;
 }
 
 const SaveJSONButton: React.FC<SaveJSONButtonProps> = ({ handleSaveJSON }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handleSaveAndShowModal = () => {
+        handleSaveJSON();
+        if (!localStorage.getItem('isf-json-saved')) {
+            setTimeout(() => {
+                setIsModalVisible(true);
+                localStorage.setItem('isf-json-saved', 'true');
+            }, 500);
+        }
+    };
+
     return (
         <div className="flex flex-col">
             <button
-                onClick={handleSaveJSON}
+                onClick={handleSaveAndShowModal}
                 className="bg-volition-purple/65 text-white font-bold py-4 px-4 mt-0 rounded transition-hover hover:bg-volition-purple/45 flex items-center justify-center"
             >
                 <FontAwesomeIcon icon={faSave} className="mr-2 text-xl flex items-center" />
                 <p className="text-center text-xl flex items-center">Save ISF JSON</p>
             </button>
             <p className="mt-1 text-center text-base text-gray-500 cursor-default">*Place this JSON alongside your mod&apos;s meta.lsx file.</p>
+            {isModalVisible && <ConfirmationModal onClose={() => setIsModalVisible(false)} />}
         </div>
     );
 };
